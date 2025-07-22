4bde2860ca56f8071feef0c3dcaada4dfbe1fbe2
kameshmathi <kameshmathi1@gmail.com>
2025-07-22 22:02:32 +0530

    this is my assignment student and product detail

---
+++ C2TC_corejavaprograms/src/entityclassassignment1/StudentDemo.java
@@ -0,0 +1,18 @@
+package entityclassassignment1;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		Student s1 = new Student(101, "Shinchan", "CSE");
+        Student s2 = new Student(102, "Heemavari", "IT");
+        Student s3 = new Student(103, "Juli", "ECE");
+
+        System.out.println("Student 1: " + s1);
+        System.out.println("Student 2: " + s2);
+        System.out.println("Student 3: " + s3);
+    }
+// TODO Auto-generated method stub
+
+	}
+
+
