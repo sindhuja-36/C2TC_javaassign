8f35e8195b60179e052c670d395b0ab18bfa921d
kameshmathi <kameshmathi1@gmail.com>
2025-07-22 15:39:47 +0530

    Hello World

---
+++ C2TC_Corejavaprogram/src/hello12/HelloWorld.java
@@ -0,0 +1,15 @@
+package hello12;
+import java.util.*;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		Scanner input= new Scanner(System.in);
+		System.out.println("Enter your name:");
+		String n= input.next();
+		System.out.println("Hello" + n);
+				
+		// TODO Auto-generated method stub
+
+	}
+
+}
