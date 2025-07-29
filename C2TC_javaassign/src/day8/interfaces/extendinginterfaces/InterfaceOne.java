656ddf757f2401d47d621c9087a709df0abbdc82
kameshmathi <kameshmathi1@gmail.com>
2025-07-29 21:25:33 +0530

    this is my assignment

---
+++ C2TC_javaassign/src/day8/interfaces/extendinginterfaces/InterfaceOne.java
@@ -0,0 +1,7 @@
+
+	package day8.interfaces.extendinginterfaces;
+
+	interface InterfaceOne{  
+		  void print();  
+		}  
+
---
+++ C2TC_javaassign/src/day8/interfaces/functionalinterfaces/GreetInterface.java
@@ -0,0 +1,6 @@
+package day8.interfaces.functionalinterfaces;
+
+@FunctionalInterface
+public interface GreetInterface {
+	public String greet();
+}
