d74c43afa4887f195a96264bd9f74342c64bf420
kameshmathi <kameshmathi1@gmail.com>
2025-07-31 16:14:32 +0530

    This is my assignment

---
+++ C2TC_javaassign/src/dayeleven/UncheckedException.java
@@ -0,0 +1,20 @@
+package dayeleven;
+
+public class UncheckedException {
+	//Program to demonstrate unchecked exception
+	
+
+	//Unchecked Exceptions are Runtime Exception
+	
+		public static void main(String[] args) {
+			int x[];
+			try {
+				x = new int[] { 1, 2, 3, 4 };
+				System.out.println(x[6]);// out of bound
+			} catch (ArrayIndexOutOfBoundsException e) {
+				System.err.println("Specified index does not exist. " + e.getMessage());// to get the detail message of																				// exception as a string value.
+
+			}
+		}
+	}
+
---
+++ C2TC_javaassign/src/dayeleven/v1/Demo.java
@@ -0,0 +1,14 @@
+package dayeleven.v1;
+
+public class Demo {
+
+	//Program without exception handling
+	
+		public static void main(String[] args) {
+			System.out.println("the program continues....");
+			int data=100/0;
+		}
+		
+	}
+
+
