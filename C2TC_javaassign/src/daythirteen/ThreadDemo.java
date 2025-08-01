83ea9e2b696fb7d1c1a643c6f59fa65c4bee7d89
kameshmathi <kameshmathi1@gmail.com>
2025-08-01 16:18:05 +0530

    this is my assignment

---
+++ C2TC_javaassign/src/daythirteen/ThreadDemo.java
@@ -0,0 +1,18 @@
+package daythirteen;
+
+public class ThreadDemo {
+	//Executor class for ChildThread
+
+		public static void main(String[] args) {
+			ChildThread threadOne = new ChildThread(5, "First");
+			ChildThread threadTwo = new ChildThread(10, "Second");
+
+			threadOne.start();
+			//threadOne.start(); // throws IllegalStateException
+			// threadOne.run(); // single threaded application 
+			threadTwo.start();
+
+			System.out.println("-----------------------End of Main--------------------------");
+		}
+	}
+
---
+++ C2TC_javaassign/src/daythirteen/ThreadLifeCycleDemo.java
@@ -0,0 +1,22 @@
+package daythirteen;
+
+public class ThreadLifeCycleDemo {
+	//Program to demonstrate Thread life cycle
+
+
+
+		public static void main(String[] args) {
+			MyChildThread myThread = new MyChildThread();
+			System.out.println("Before Runnable stage Thread is alive or not? " + myThread.isAlive());
+			myThread.start();
+
+			try {
+				Thread.sleep(4000);
+			} catch (InterruptedException exp) {
+				System.err.println("Thread is interrupted !");
+			}
+
+			System.out.println("After complete execution of Thread ,it is alive or not? " + myThread.isAlive());
+		}
+	}
+
