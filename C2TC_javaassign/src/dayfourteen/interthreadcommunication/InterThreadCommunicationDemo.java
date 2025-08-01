8b81f63c335df9f6661eb6729d85ed841a44052b
kameshmathi <kameshmathi1@gmail.com>
2025-08-01 22:34:46 +0530

    this is  my Assignment

---
+++ C2TC_javaassign/src/dayfourteen/interthreadcommunication/InterThreadCommunicationDemo.java
@@ -0,0 +1,20 @@
+package dayfourteen.interthreadcommunication;
+
+public class InterThreadCommunicationDemo {
+
+	public static void main(String[] args) {
+		
+		Q obj=new Q();
+		
+		Producer p1=new Producer(obj); //Producer thread
+		Consumer c1=new Consumer(obj); //Consumer thread
+					
+		try {
+			p1.join();
+			c1.join();
+		} catch (InterruptedException e) {
+			System.out.println(e);
+		}
+	}
+
+}
